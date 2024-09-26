6d78972af2baa104f1becfc72771281fced5d3f2
Rafael Alves <rafael.alves@vilt-group.com>
2024-09-26 00:37:06 -0300

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="h-screen flex items-center justify-center">
+    <p className="text-8xl font-mono">
+      gigi ❤️
+    </p>
+   </div>
+  );
+}
